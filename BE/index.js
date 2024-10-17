8a43abc276e888d9bba43fc751a51b80935ff842
Juan Quintero <quinterojuan0246@gmail.com>
2024-10-17 20:42:51 -0300

    feature: .env urls

--- BE/index.js
+++ BE/index.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const express = require("express");
 const axios = require("axios");
 const app = express();
 const port = 8000;
 
+const nagerDateUrl = process.env.NAGER_DATE_URL;
+const postmanUrl = process.env.POSTMAN_URL;
+
 app.get("/countries", async (req, res) => {
     try {
-        const response = await axios.get(
-            "https://date.nager.at/api/v3/AvailableCountries"
-        );
+        const response = await axios.get(`${nagerDateUrl}/AvailableCountries`);
         res.json(response.data);
     } catch (error) {
         res.status(500).send("Error fetching available countries");
@@ -19,27 +21,24 @@ app.get("/countries/:countryCode", async (req, res) => {
     console.log(`looking for country information with code: ${countryCode}`);
     try {
         const countryInfoResponse = await axios.get(
-            `https://date.nager.at/api/v3/CountryInfo/${countryCode}`
+            `${nagerDateUrl}/CountryInfo/${countryCode}`
         );
         const borderCountries = countryInfoResponse.data.borders;
 
-        const populationResponse = await axios.post(
-            "https://countriesnow.space/api/v0.1/countries/population",
-            {
-                country: countryInfoResponse.data.commonName,
-            }
-        );
+        const populationResponse = await axios.post(`${postmanUrl}/population`, {
+            country: countryInfoResponse.data.commonName,
+        });
         const populationData = populationResponse.data.data.populationCounts;
 
-        const flagResponse = await axios.post("https://countriesnow.space/api/v0.1/countries/flag/images", {
-            iso2: countryCode
+        const flagResponse = await axios.post(`${postmanUrl}/flag/images`, {
+            iso2: countryCode,
         });
         const flagUrl = flagResponse.data.data.flag;
 
         const countryData = {
             borderCountries,
             populationData,
-            flagUrl
+            flagUrl,
         };
 
         res.json(countryData);
