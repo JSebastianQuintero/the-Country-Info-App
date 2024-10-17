c244117bd9f19ab6004398e38b7fa261873ff386
Juan Quintero <quinterojuan0246@gmail.com>
2024-10-17 20:20:50 -0300

    feature: populationData added in deatailed information endpoint

--- BE/index.js
+++ BE/index.js
@@ -4,32 +4,45 @@ const app = express();
 const port = 8000;
 
 app.get("/countries", async (req, res) => {
-        try {
-                const response = await axios.get(
-                        "https://date.nager.at/api/v3/AvailableCountries"
-                );
-                res.json(response.data);
-        } catch (error) {
-                res.status(500).send("Error fetching available countries");
-        }
+    try {
+        const response = await axios.get(
+            "https://date.nager.at/api/v3/AvailableCountries"
+        );
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send("Error fetching available countries");
+    }
 });
 
 app.get("/countries/:countryCode", async (req, res) => {
-        const { countryCode } = req.params;
-        console.log(
-                `looking for country information with code: ${countryCode}`
+    const { countryCode } = req.params;
+    console.log(`looking for country information with code: ${countryCode}`);
+    try {
+        const countryInfoResponse = await axios.get(
+            `https://date.nager.at/api/v3/CountryInfo/${countryCode}`
         );
-        try {
-                const countryInfoResponse = await axios.get(
-                        `https://date.nager.at/api/v3/CountryInfo/${countryCode}`
-                );
-                const borderCountries = countryInfoResponse.data.borders;
-                res.json(borderCountries);
-        } catch (error) {
-                res.status(500).send("Error fetching country information");
-        }
+        const borderCountries = countryInfoResponse.data.borders;
+
+        const populationResponse = await axios.post(
+            "https://countriesnow.space/api/v0.1/countries/population",
+            {
+                country: countryInfoResponse.data.commonName,
+            }
+        );
+        const populationData = populationResponse.data.data.populationCounts;
+
+
+        const countryData = {
+            borderCountries,
+            populationData
+        };
+
+        res.json(countryData);
+    } catch (error) {
+        res.status(500).send("Error fetching country information");
+    }
 });
 
 app.listen(port, () => {
-        console.log(`Country API listening on port ${port} :D`);
+    console.log(`Country API listening on port ${port} :D`);
 });
