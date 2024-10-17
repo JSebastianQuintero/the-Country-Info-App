bb6dde3e26ae65f13e8576e0b295d0ab13562054
Juan Quintero <quinterojuan0246@gmail.com>
2024-10-17 20:05:50 -0300

    feature: Retrieve detailed information endpoint

--- BE/index.js
+++ BE/index.js
@@ -4,15 +4,32 @@ const app = express();
 const port = 8000;
 
 app.get("/countries", async (req, res) => {
-    try {
-        const response = await axios.get("https://date.nager.at/api/v3/AvailableCountries");
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send("Error fetching available countries");
-    }
+        try {
+                const response = await axios.get(
+                        "https://date.nager.at/api/v3/AvailableCountries"
+                );
+                res.json(response.data);
+        } catch (error) {
+                res.status(500).send("Error fetching available countries");
+        }
+});
+
+app.get("/countries/:countryCode", async (req, res) => {
+        const { countryCode } = req.params;
+        console.log(
+                `looking for country information with code: ${countryCode}`
+        );
+        try {
+                const countryInfoResponse = await axios.get(
+                        `https://date.nager.at/api/v3/CountryInfo/${countryCode}`
+                );
+                const borderCountries = countryInfoResponse.data.borders;
+                res.json(borderCountries);
+        } catch (error) {
+                res.status(500).send("Error fetching country information");
+        }
 });
 
 app.listen(port, () => {
-    console.log(`Country API listening on port ${port} :D`);
+        console.log(`Country API listening on port ${port} :D`);
 });
-
